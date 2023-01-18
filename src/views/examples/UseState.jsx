f3eb4d6c1694e1fee17fd3a266c1a7b7b8e67fbb
Marcio Bueno <mabuen@live.com>
2023-01-18 15:07:10 +0100

    Update UseState.jsx

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from "../../components/layout/SectionTitle"
 
 const UseState = (props) => {
+    const [count,setCount]=useState(0)
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #1"></SectionTitle>
+            <div className="center">
+                
+            </div>
+            <SectionTitle title="Exercício #2"></SectionTitle>
         </div>
     )
 }
